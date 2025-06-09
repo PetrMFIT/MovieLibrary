d47587edb167aff3688b3d6fc107bca69f942faa
PetrMFIT <xmolikp00@stud.fit.vutbr.cz>
2025-06-09 21:04:16 +0200

    MoviesController, simple index

---
+++ MovieLibrary/Controllers/MoviesController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MovieLibrary.Data;
+
+namespace MovieLibrary.Controllers
+{
+    public class MoviesController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public MoviesController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var movies = _context.Movies.ToList();
+            return View(movies);
+        }
+    }
+}
